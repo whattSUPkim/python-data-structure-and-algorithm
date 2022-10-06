adbc53ea127e6f5cfba7972434d08b9a4c4ca5b0
김원섭 <whattsup_kim@naver.com>
2022-10-06 11:14:46 +0900

    Add: algorithm

---
+++ Practice/Programmers/Lv2-땅따먹기.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12913#
+def solution(land):
+    for i in range(1, len(land)):
+        land[i][0] = land[i][0] + max(land[i - 1][1], land[i - 1][2], land[i - 1][3])
+        land[i][1] = land[i][1] + max(land[i - 1][0], land[i - 1][2], land[i - 1][3])
+        land[i][2] = land[i][2] + max(land[i - 1][0], land[i - 1][1], land[i - 1][3])
+        land[i][3] = land[i][3] + max(land[i - 1][0], land[i - 1][1], land[i - 1][2])
+    return max(land[-1])
