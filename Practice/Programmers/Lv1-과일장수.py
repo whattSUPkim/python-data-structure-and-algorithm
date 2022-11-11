f368a87fe7b97b4630abdb838905d2fc627179b9
김원섭 <whattsup_kim@naver.com>
2022-11-11 10:26:59 +0900

    Add: algorithm

---
+++ Practice/Programmers/Lv1-과일장수.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/135808
+def solution(k, m, score):
+    answer = 0
+    score = sorted(score, reverse=True)
+    for i in range(m - 1, len(score), m):
+        answer += score[i] * m
+
+    return answer
